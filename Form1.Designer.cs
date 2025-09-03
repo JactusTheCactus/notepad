ecd75ef6fe729eb87a1eacb76303d46a8da68129
JactusTheCactus (Laptop) <threateninglyamusing@gmail.com>
2025-09-02 23:40:54 -0700

    -

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,29 +30,57 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.rtbEditor = new System.Windows.Forms.RichTextBox();
             this.formattingButton = new System.Windows.Forms.Button();
+            this.ViewMode = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // rtbEditor
             // 
+            this.rtbEditor.AcceptsTab = true;
             resources.ApplyResources(this.rtbEditor, "rtbEditor");
             this.rtbEditor.Name = "rtbEditor";
-            this.rtbEditor.TextChanged += new System.EventHandler(this.rtbEditor_TextChanged);
             // 
             // formattingButton
             // 
             resources.ApplyResources(this.formattingButton, "formattingButton");
             this.formattingButton.Name = "formattingButton";
             this.formattingButton.UseVisualStyleBackColor = true;
-            this.formattingButton.Click += new System.EventHandler(this.button1_Click);
+            this.formattingButton.Click += new System.EventHandler(this.formattingButton_Click);
+            // 
+            // ViewMode
+            // 
+            resources.ApplyResources(this.ViewMode, "ViewMode");
+            this.ViewMode.Name = "ViewMode";
+            this.ViewMode.ReadOnly = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.formattingButton);
+            this.panel1.Controls.Add(this.ViewMode);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.rtbEditor);
+            this.panel2.Controls.Add(this.panel1);
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
             // 
             // Form1
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.formattingButton);
-            this.Controls.Add(this.rtbEditor);
+            this.Controls.Add(this.panel2);
             this.Name = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
 			}
@@ -61,5 +89,8 @@
 
 		private System.Windows.Forms.RichTextBox rtbEditor;
 		private System.Windows.Forms.Button formattingButton;
-		}
+		private System.Windows.Forms.RichTextBox ViewMode;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+	}
 	}
--- Form1.cs
+++ Form1.cs
@@ -10,88 +10,62 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 namespace Notepad
-	{
+{
 	public partial class Form1 : Form
-		{
+	{
+		String editingLabel = "Editing Mode";
+		String formattingLabel = "Formatting Mode";
+		string rawText = "";
+		bool formatted = false; // your toggle
 		public Form1()
-			{
+		{
 			InitializeComponent();
-			}
+		}
 		private void SetStyle(String RegExp, FontStyle style)
-			{
+		{
 			foreach (Match match in Regex.Matches(rtbEditor.Text, RegExp))
-				{
+			{
 				rtbEditor.Select(match.Index, match.Length);
 				rtbEditor.SelectionFont = new Font("Segoe UI", 20, style);
 				rtbEditor.DeselectAll();
-				}
-			}
-		bool formatted = false;
-		private void ApplyMarkup(string pattern, FontStyle style)
-			{
-			foreach (Match match in Regex.Matches(rtbEditor.Text, pattern))
-				{
-				string innerText = match.Groups[1].Value;
-
-				// Apply style to inner text (not the tags)
-				int start = match.Index + match.Value.IndexOf(innerText);
-				rtbEditor.Select(start, innerText.Length);
-				rtbEditor.SelectionFont = new Font("Segoe UI", 20, style);
-				}
-			}
-		private void HideTags(string tagPattern)
-			{
-			foreach (Match match in Regex.Matches(rtbEditor.Text, tagPattern))
-				{
-				rtbEditor.Select(match.Index, match.Length);
-				rtbEditor.Text = Regex.Replace(rtbEditor.Text, @"</?\w+>", "");
-				//rtbEditor.SelectionColor = rtbEditor.BackColor; // invisible
-				}
-			}
-		private void ApplyFormatting()
-			{
-			int cursorPos = rtbEditor.SelectionStart;
-
-			// Reset formatting
-			rtbEditor.SelectAll();
-			rtbEditor.SelectionFont = new Font("Segoe UI", 20, FontStyle.Regular);
-			rtbEditor.SelectionColor = Color.Black;
-			rtbEditor.DeselectAll();
-
-			// Apply bold
-			ApplyMarkup(@"<b>(.*?)</b>", FontStyle.Bold);
-
-			// Apply italic
-			ApplyMarkup(@"<i>(.*?)</i>", FontStyle.Italic);
-
-			// Hide tags
-			HideTags(@"</?b>");
-			HideTags(@"</?i>");
-
-			rtbEditor.SelectionStart = cursorPos;
-			}
-		private void rtbEditor_TextChanged(object sender, EventArgs e)
-			{
-			if (formatted)
-				{
-				ApplyFormatting();
-				}
-			}
-		private void button1_Click(object sender, EventArgs e)
-			{
-			formatted = !formatted;
-			if (formatted)
-				{
-				ApplyFormatting();
-				}
-			else
-				{
-				string plainText = rtbEditor.Text;
-				rtbEditor.SelectAll();
-				rtbEditor.SelectionFont = new Font("Segoe UI", 20, FontStyle.Regular);
-				rtbEditor.SelectionColor = Color.Black;
-				rtbEditor.DeselectAll();
-				}
 			}
 		}
+		private void formattingButton_Click(object sender, EventArgs e)
+		{
+			formatted = !formatted;
+
+			if (formatted)
+			{
+				ViewMode.Text = formattingLabel;
+				// Copy current text to rawText and apply formatting
+				rawText = rtbEditor.Text;
+				rtbEditor.Text = ApplyFormatting(rawText);
+
+				// Prevent further input
+				rtbEditor.ReadOnly = true;
+			}
+			else
+			{
+				ViewMode.Text = editingLabel;
+				// Restore raw text for editing
+				rtbEditor.Text = rawText;
+
+				// Allow typing again
+				rtbEditor.ReadOnly = false;
+			}
+
+			// Keep caret at the end
+			rtbEditor.SelectionStart = rtbEditor.Text.Length;
+			rtbEditor.ScrollToCaret();
+		}
+		private string ApplyFormatting(string input)
+		{
+			// Example formatting: uppercase
+			return input.ToUpper();
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			ViewMode.Text = editingLabel;
+		}
 	}
+}
