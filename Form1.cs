57b4f73a459e0eb3c711f8c8ce1dd3bb064ecb76
JactusTheCactus (Laptop) <threateninglyamusing@gmail.com>
2025-09-03 02:47:03 -0700

    -

--- Form1.cs
+++ Form1.cs
@@ -2,68 +2,71 @@
 using System.Drawing;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 namespace Notepad
 {
 	public partial class Form1 : Form
 	{
-		String editingLabel = "Editing Mode";
-		String formattingLabel = "Formatting Mode";
 		string rawText = "";
 		bool formatted = false; // your toggle
 		public Form1()
 		{
 			InitializeComponent();
 		}
-		private void SetStyle(String RegExp, FontStyle style)
+		private void RemoveTags(string pattern)
 		{
-			foreach (Match match in Regex.Matches(rtbEditor.Text, RegExp))
+			var matches = Regex.Matches(rtbEditor.Text, pattern);
+			for (int i = matches.Count - 1; i >= 0; i--)
+			{
+				var match = matches[i];
+				rtbEditor.Select(match.Index, match.Length);
+				rtbEditor.SelectedText = ""; // safely remove tag
+			}
+		}
+		private void SetStyle(String tag, FontStyle style)
+		{
+			String pattern = $@"(?<=<{tag}>)(.*?)(?=</{tag}>)";
+			foreach (Match match in Regex.Matches(rtbEditor.Text, pattern))
 			{
 				rtbEditor.Select(match.Index, match.Length);
 				rtbEditor.SelectionFont = new Font("Segoe UI", 20, style);
 				rtbEditor.DeselectAll();
+			}
+			RemoveTags($@"</?{tag}>");
+		}
+		private void format()
+		{
+			if (formatted)
+			{
+				rtbEditor.Font = new Font("Arial", 20, FontStyle.Regular);
+				ViewMode.Text = "Formatting Mode";
+				rawText = rtbEditor.Text;
+				SetStyle("i", FontStyle.Italic);
+				SetStyle("b", FontStyle.Bold);
+				Console.WriteLine(rtbEditor.Text);
+				rtbEditor.ReadOnly = true;
+			}
+			else
+			{
+				ViewMode.Text = "Editing Mode";
+				rtbEditor.Text = rawText;
+				rtbEditor.Font = new Font("Consolas", 20, FontStyle.Regular);
+				rtbEditor.ReadOnly = false;
 			}
 		}
 		private void formattingButton_Click(object sender, EventArgs e)
 		{
 			formatted = !formatted;
 
-			if (formatted)
-			{
-				rtbEditor.Font = new Font("Arial", 20, FontStyle.Regular);
-				ViewMode.Text = formattingLabel;
-				// Copy current text to rawText and apply formatting
-				rawText = rtbEditor.Text;
-				SetStyle(@"(?<=<i>)(.*?)(?=</i>)", FontStyle.Italic);
-				//Regex.Replace(rtbEditor.Text, @"<(\w+)>(.*?)</\1>", @"$2");
-				rtbEditor.Text.Replace(@"</?\w+>", @"");
-				//rtbEditor.Text = ApplyFormatting(rawText);
-
-				// Prevent further input
-				rtbEditor.ReadOnly = true;
-			}
-			else
-			{
-				ViewMode.Text = editingLabel;
-				// Restore raw text for editing
-				rtbEditor.Text = rawText;
-				rtbEditor.Font = new Font("Consolas", 20, FontStyle.Regular);
-
-				// Allow typing again
-				rtbEditor.ReadOnly = false;
-			}
+			format();
 
 			// Keep caret at the end
 			rtbEditor.SelectionStart = rtbEditor.Text.Length;
 			rtbEditor.ScrollToCaret();
 		}
-		private string ApplyFormatting(string input)
-		{
-			// Example formatting: uppercase
-			return input.ToUpper();
-		}
 		private void Form1_Load(object sender, EventArgs e)
 		{
-			ViewMode.Text = editingLabel;
+			format();
 		}
 	}
 }
